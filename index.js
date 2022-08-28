b5044cdf9397b7d91b8708830c53ca54dc6bda3d
saroj3 <bhattaraisaroj91@gmail.com>
2022-08-28 11:29:17 +0900

    add in index

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import express from "express";
+import bodyParser from "body-parser";
 var app = express();
 
-app.listen(3000,(req, res) => {
-console.log('server is runnig on 3000');
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+let user =['suman', 12]
+
+app.get("/user",(req,res)=>{
+    res.send(user)
+})
+app.listen(3000,(req, res) =>{
+    console.log('server is running on 3000');
 })
